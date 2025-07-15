d7fb5cfe1c15a212d875fe103f8fada80ee0ac3f
Mah Yock Gen <yock.gen.mah@intel.com>
2025-07-15 17:11:58 +0800

    Adding overlay configuration init version - many hardcoded steps need to resolved
    Signed-off-by: Mah Yock Gen <yock.gen.mah@intel.com>

--- internal/image/imagesecure/imagesecure.go
+++ internal/image/imagesecure/imagesecure.go
@@ -3,8 +3,14 @@ package imagesecure
 import (
 	"strings"
 
+	"fmt"
+
+	"path/filepath"
+
 	"github.com/open-edge-platform/image-composer/internal/config"
+	"github.com/open-edge-platform/image-composer/internal/utils/file"
 	"github.com/open-edge-platform/image-composer/internal/utils/logger"
+	"github.com/open-edge-platform/image-composer/internal/utils/shell"
 )
 
 func ConfigImageSecurity(installRoot string, template *config.ImageTemplate) error {
@@ -32,9 +38,147 @@ func ConfigImageSecurity(installRoot string, template *config.ImageTemplate) err
 		return nil
 	}
 
-	// updateImageFstab has set the rootfs to read-only in fstab - not additional code required
-	// TODO: mounting overlay
+	// Mounting overlay for read write directories
+	if err := configOverlay(installRoot, template); err != nil {
+		return fmt.Errorf("failed to configure overlay: %w", err)
+	}
 
 	log.Debugf("Root filesystem made read-only successfully")
 	return nil
+}
+
+func configOverlay(installRoot string, template *config.ImageTemplate) error {
+
+	log := logger.Logger()
+	log.Debugf("Configuring overlay for read-only root filesystem is not implemented yet")
+
+	ovlyDir, err := prepareOverlayDir(installRoot)
+	if err != nil {
+		return fmt.Errorf("failed to prepare ESP directory: %w", err)
+	}
+	log.Debugf("Succesfully Creating Overlay Path:", ovlyDir)
+
+	err = updateImageFstab(installRoot)
+	if err != nil {
+		return fmt.Errorf("failed to update fstab: %w", err)
+	}
+	log.Debugf("Succesfully Updating fstab for overlay")
+
+	err = createOverlayMntSvc(installRoot)
+	if err != nil {
+		return fmt.Errorf("failed to create overlay mounting service: %w", err)
+	}
+	log.Debugf("Succesfully Created overlay mounting service")
+
+	return nil
+}
+
+func createOverlayMntSvc(installRoot string) error {
+	log := logger.Logger()
+
+	scriptLines := []string{
+		"#!/bin/bash",
+		"",
+		"if [ ! -d /opt/overlay/etc/upper ]; then",
+		"    echo \"Missing /opt/overlay/etc/upper\"",
+		"    exit 1",
+		"fi",
+		"",
+		"if [ ! -d /ro/etc ]; then",
+		"    echo \"Missing /ro/etc\"",
+		"    exit 1",
+		"fi",
+		"",
+		"# Bind mount rootfs /etc to lowerdir",
+		"mount --bind /etc /ro/etc",
+		"",
+		"# Mount overlay",
+		"mount -t overlay overlay -o lowerdir=/ro/etc,upperdir=/opt/overlay/etc/upper,workdir=/opt/overlay/etc/work /etc",
+	}
+	scriptContent := strings.Join(scriptLines, "\n") + "\n"
+
+	mountScriptPath := filepath.Join(installRoot, "usr", "local", "bin", "setup-overlay.sh")
+	err := file.Append(scriptContent, mountScriptPath)
+	if err != nil {
+		return fmt.Errorf("failed to append to mountScriptPath: %w", err)
+	}
+
+	//grant execute permissions to the script
+	chmodCmd := fmt.Sprintf("chmod -R 755 %s", filepath.Dir(mountScriptPath))
+	if _, err = shell.ExecCmd(chmodCmd, true, "", nil); err != nil {
+		return fmt.Errorf("failed to set permissions for overlay mounting script: %w", err)
+	}
+
+	//create service
+	svcLines := []string{
+		"[Unit]",
+		"Description=Set up OverlayFS for /etc",
+		"Requires=opt.mount",
+		"After=opt.mount",
+		"",
+		"[Service]",
+		"Type=oneshot",
+		"ExecStart=/usr/local/bin/setup-overlay.sh",
+		"RemainAfterExit=true",
+		"",
+		"[Install]",
+		"WantedBy=multi-user.target",
+	}
+	svcContent := strings.Join(svcLines, "\n") + "\n"
+
+	svcPath := filepath.Join(installRoot, "etc", "systemd", "system", "setup-overlay.service")
+	err = file.Append(svcContent, svcPath)
+	if err != nil {
+		return fmt.Errorf("failed to append to service file: %w", err)
+	}
+	// Enable the overlay mounting service
+	enableCmd := `bash -c "systemctl enable setup-overlay.service"`
+	if _, err = shell.ExecCmd(enableCmd, true, installRoot, nil); err != nil {
+		return fmt.Errorf("failed to enable overlay mounting service: %w", err)
+	}
+	log.Debugf("Updated mountScriptPath with overlay settings")
+	return nil
+}
+
+func updateImageFstab(installRoot string) error {
+	log := logger.Logger()
+
+	lines := []string{
+		"", // An empty string for the blank line
+		"/opt/var/log /var/log none bind 0 0",
+		"/opt/var/lib /var/lib none bind 0 0",
+		"", // An empty string for the blank line
+		"tmpfs /tmp tmpfs mode=1777,nosuid,nodev 0 0",
+		"tmpfs /run tmpfs mode=0755,nosuid,nodev 0 0",
+	}
+	contentToAppend := strings.Join(lines, "\n") + "\n" // Add a final newline if needed
+
+	fstabFullPath := filepath.Join(installRoot, "etc", "fstab")
+	err := file.Append(contentToAppend, fstabFullPath)
+	if err != nil {
+		return fmt.Errorf("failed to append to fstab: %w", err)
+	}
+
+	log.Debugf("Updated fstab with overlay settings")
+	return nil
+}
+
+func prepareOverlayDir(installRoot string) (string, error) {
+	dirs := []string{
+		"/opt/overlay/etc/",
+		"/opt/overlay/etc/upper",
+		"/opt/overlay/etc/work",
+		"/ro/etc",
+	}
+
+	// Create required overlay directories
+	for _, dir := range dirs {
+		cmd := fmt.Sprintf("mkdir -p %s", dir)
+		if _, err := shell.ExecCmd(cmd, true, installRoot, nil); err != nil {
+			return "", err
+		}
+	}
+
+	// Return the overlay directory
+	return dirs[0], nil
 }
